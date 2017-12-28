5e05e4d7dfa9b172ae80a223736dcd32fc4d182f
Fraz <diman896@gmail.com>
2017-12-28 23:37:21 +0300

    Fuse refactoring.

--- task5/lab5.c
+++ task5/lab5.c
@@ -10,313 +10,281 @@
 #include <stdlib.h>
 #include <errno.h>
 
-/* foward structure declaration (fs_node_t and fs_dir_node_t cross-reference each other) */
-struct fs_node_s;
+struct node;
 
-typedef enum fs_node_type_e {
-    FS_NODE_FILE,
-    FS_NODE_DIRECTORY
-} fs_node_type_t;
+typedef enum node_type {
+	FILE,
+	DIRECTORY
+} node_type;
 
-/* file-related information */
-typedef struct fs_file_node_s {
-    int size;
-    char *data_ptr;
-} fs_file_node_t;
+typedef struct file_node {
+	int size;
+	char *data;
+} file_node_t;
 
-/* directory-related information */
-typedef struct fs_dir_node_s {
-    struct fs_node_s *child;
-} fs_dir_node_t;
+typedef struct dir_node {
+	struct node *first_child;
+} dir_node_t;
 
-/* structure representing a node in our FS tree */
-typedef struct fs_node_s {
-    char *name;
-    struct fs_node_s *next_sibling;
-    fs_node_type_t type;
-    mode_t mode;
-    int n_links;
+typedef struct node {
+	char *name;
+	struct node *next_sibling;
+	node_type type;
+	mode_t mode;
+	int links_count;
 
-    union {
-        fs_file_node_t file;
-        fs_dir_node_t dir;
-    } info;
+	union {
+		file_node_t file;
+		dir_node_t dir;
+	} content;
 
-} fs_node_t;
+} node_t;
 
-static fs_node_t *root;
+static node_t *root;
 
-/* Find a node in our virtual FS tree corresponding to the specified path. */
-static fs_node_t *find_node(const char *path, fs_node_t *parent) {
-    int entry_len = 0;
-    fs_node_t *current, *result = NULL;
+static node_t *findNode(const char *path, node_t *parent) {
+	int entry_len = 0;
+	node_t *current, *result = NULL;
 
-    printf("find %s %p\n", path, parent);
+	while (*path != '\0' && *path == '/') {
+		path++;
+	}
 
-    /* skip leading '/' symbols */
-    while (*path != '\0' && *path == '/') {
-        path++;
-    }
+	while (path[entry_len] != '\0' &&
+		path[entry_len] != '/') {
+		entry_len++;
+	}
 
-    /* calculate the length of the current path entry */
-    while (path[entry_len] != '\0' &&
-           path[entry_len] != '/') {
-        entry_len++;
-    }
+	if (parent == NULL || parent->type != DIRECTORY) {
+		result = parent;
+	}
+	else if (entry_len == 0) {
+		result = parent;
+	}
+	else {
+		current = parent->content.dir.first_child;
+		while (current != NULL && strncmp(current->name, path, entry_len)) {
+			current = current->next_sibling;
+		}
 
-    if (parent == NULL || parent->type != FS_NODE_DIRECTORY) {
-        /* 'parent' must represent a directory */
-        result = parent;
-    } else if (entry_len == 0) {
-        /* if the path is empty (e.g. "/" or ""), return parent */
-        result = parent;
-    } else {
-        /* traverse children in search for the next entry */
-        current = parent->info.dir.child;
-        while (current != NULL && strncmp(current->name, path, entry_len)) {
-            current = current->next_sibling;
-        }
+		if (current != NULL) {
+			result = findNode(path + entry_len, current);
+		}
+	}
 
-        if (current != NULL) {
-            result = find_node(path + entry_len, current);
-        }
-    }
-
-    return result;
+	return result;
 }
 
-static int readDir(const char *path,
-                   void *buffer,
-                   fuse_fill_dir_t filler,
-                   off_t offset,
-                   struct fuse_file_info *fi) {
-    fs_node_t *parent_node, *current_node;
-
-    printf("readDir: %s\n", path);
-
-    filler(buffer, ".", NULL, 0);
-    filler(buffer, "..", NULL, 0);
-
-    parent_node = find_node(path, root);
-
-    if (parent_node != NULL) {
-        printf("dir found, n_links %d\n", parent_node->n_links);
-
-        current_node = parent_node->info.dir.child;
-        while (current_node != NULL) {
-            filler(buffer, current_node->name, NULL, 0);
-            current_node = current_node->next_sibling;
-        }
-    } else {
-        printf("node not found!\n");
-    }
-
-    return 0;
+static void add_child_node(node_t *parent, node_t *child) {
+	child->next_sibling = parent->content.dir.first_child;
+	parent->content.dir.first_child = child;
 }
 
-static int getAttr(const char *path, struct stat *st) {
-    fs_node_t *node;
-    int ret = 0;
+static node_t *create_directory(char *name) {
+	node_t *node;
 
-    printf("getAttr: %s\n", path);
+	node = (node_t *)malloc(sizeof(node_t));
 
-    node = find_node(path, root);
-    if (node != NULL) {
-        st->st_mode = node->mode;
-        if (node->type == FS_NODE_DIRECTORY) {
-            st->st_mode |= S_IFDIR;
-        } else if (node->type == FS_NODE_FILE) {
-            st->st_mode |= S_IFREG;
-            st->st_size = node->info.file.size;
-        }
+	node->name = name;
+	node->mode = 0777;
+	node->type = DIRECTORY;
+	node->next_sibling = NULL;
+	node->content.dir.first_child = NULL;
+	node->links_count = 2;
 
-        //st->st_nlink = node->n_links;
-        st->st_uid = getuid();
-        st->st_gid = getgid();
-    } else {
-        printf("node not found!");
-        ret = -ENOENT;
-    }
-
-    return ret;
+	return node;
 }
 
-static int readFile(const char *path,
-                    char *buffer,
-                    size_t size,
-                    off_t offset,
-                    struct fuse_file_info *fi) {
-    fs_node_t *node;
-    int bytes_read = 0;
-
-    node = find_node(path, root);
-    if (node != NULL && offset < node->info.file.size) {
-        int bytes_available = node->info.file.size - offset;
-        if (bytes_available >= size) {
-            bytes_read = size;
-        } else {
-            bytes_read = bytes_available;
-        }
-
-        if (bytes_read > 0) {
-            memcpy(buffer, node->info.file.data_ptr, bytes_read);
-        }
-    }
-
-    return bytes_read;
+static node_t *create_directory_and_append_to_parent(char *path, mode_t mode, node_t *parent) {
+	node_t *d = create_directory(path);
+	parent->links_count++;
+	add_child_node(parent, d);
+	return d;
 }
 
-static int mkDir(const char *path, mode_t mode);
+static node_t *create_file(char *name) {
+	node_t *node;
 
-static int chMod(const char *path, mode_t mode) {
-    fs_node_t *node = find_node(path, root);
-    if (node != NULL) {
-        node->mode = mode;
-        return 0;
-    }
-    return -1;
+	node = (node_t *)malloc(sizeof(node_t));
+
+	node->name = name;
+	node->mode = 0666;
+	node->type = FILE;
+	node->next_sibling = NULL;
+	node->content.file.size = 0;
+	node->content.file.data = NULL;
+	node->links_count = 0;
+
+	return node;
 }
 
-static void add_child_node(fs_node_t *parent, fs_node_t *child);
+static int copy_file(const char *path) {
+	FILE* input, *output;
+	char buffer[10];
+	size_t nread;
+
+	node_t* file = create_file("cat");
+	input = fopen("/bin/cat", "rb");
+	while (nread = fread(buffer, sizeof(char), sizeof(buffer), input)) {
+		fwrite(buffer, sizeof(char), nread, file->content.file.data);
+		file->content.file.size += nread;
+	}
+	file->links_count++;
+	add_child_node(root, file);
+}
 
 static struct fuse_operations operations = {
-        .getattr    = getAttr,
-        .readdir    = readDir,
-        .read     = readFile,
-        .mkdir    = mkDir,
-        .chmod   = chMod
+	.getattr = getattr,
+	.readdir = readdir,
+	.read = read,
+	.mkdir = mkdir,
+	.chmod = chmod
 };
 
-static fs_node_t *create_directory(char *name) {
-    fs_node_t *node;
+static int getattr(const char *path, struct stat *st) {
+	node_t *node;
 
-    node = (fs_node_t *) malloc(sizeof(fs_node_t));
+	node = findNode(path, root);
+	if (node != NULL) {
+		st->st_mode = node->mode;
+		if (node->type == DIRECTORY) {
+			st->st_mode |= S_IFDIR;
+		}
+		else if (node->type == FILE) {
+			st->st_mode |= S_IFREG;
+			st->st_size = node->content.file.size;
+		}
 
-    /* set default values */
-    node->name = name;
-    node->mode = 0777;
-    node->type = FS_NODE_DIRECTORY;
-    node->next_sibling = NULL;
-    node->info.dir.child = NULL;
-    node->n_links = 2; /* . and .. */
+		//st->st_nlink = node->n_links;
+		st->st_uid = getuid();
+		st->st_gid = getgid();
+	}
+	else {
+		return -ENOENT;
+	}
 
-    return node;
+	return 0;
 }
 
-static fs_node_t *create_dir(char *path, mode_t mode, fs_node_t *parent) {
-    fs_node_t *d = create_directory(path);
-    parent->n_links++;
-    add_child_node(parent, d);
-    return d;
-}
+static int readdir(const char *path,
+	void *buffer,
+	fuse_fill_dir_t filler,
+	off_t offset,
+	struct fuse_file_info *fi) {
+	node_t *parent_node, *current_node;
+
+	filler(buffer, ".", NULL, 0);
+	filler(buffer, "..", NULL, 0);
+
+	parent_node = findNode(path, root);
+
+	if (parent_node != NULL) {
+
+		current_node = parent_node->content.dir.first_child;
+		while (current_node != NULL) {
+			filler(buffer, current_node->name, NULL, 0);
+			current_node = current_node->next_sibling;
+		}
+		return 0;
+	}
 
 
-static fs_node_t *create_file(char *name) {
-    fs_node_t *node;
+	static int read(const char *path,
+		char *buffer,
+		size_t size,
+		off_t offset,
+		struct fuse_file_info *fi) {
+		node_t *node;
+		int bytes_read = 0;
 
-    node = (fs_node_t *) malloc(sizeof(fs_node_t));
+		node = findNode(path, root);
+		if (node != NULL && offset < node->content.file.size) {
+			int bytes_available = node->content.file.size - offset;
+			if (bytes_available >= size) {
+				bytes_read = size;
+			}
+			else {
+				bytes_read = bytes_available;
+			}
 
-    /* set default values */
-    node->name = name;
-    node->mode = 0666;
-    node->type = FS_NODE_FILE;
-    node->next_sibling = NULL;
-    node->info.file.size = 0;
-    node->info.file.data_ptr = NULL;
-    node->n_links = 0;
+			if (bytes_read > 0) {
+				memcpy(buffer, node->content.file.data, bytes_read);
+			}
+		}
 
-    return node;
-}
-
-static void add_child_node(fs_node_t *parent, fs_node_t *child) {
-    /* assuming parent is always a directory */
-
-    child->next_sibling = parent->info.dir.child;
-    parent->info.dir.child = child;
-}
-
-static int copyFile(const char *path){
-    FILE* input, * output;
-    char buffer[10];
-    size_t nread;
-
-    fs_node_t* file = create_file("cat");
-    input = fopen("/bin/cat", "rb");
-    while(nread = fread(buffer, sizeof(char), sizeof(buffer), input)) {
-        fwrite(buffer, sizeof(char), nread, file->info.file.data_ptr);
-        file->info.file.size+=nread;
-    }
-    file->n_links++; /* a new hard link */
-    add_child_node(root, file);
-}
+		return bytes_read;
+	}
 
 
-static void make_sample_hierarchy() {
-    fs_node_t *first, *second;
-    fs_node_t *file;
-    fs_node_t *first_inner;
+	static int chmod(const char *path, mode_t mode) {
+		node_t *node = findNode(path, root);
+		if (node != NULL) {
+			node->mode = mode;
+			return 0;
+		}
+		return -1;
+	}
 
-    first = create_dir("first", 0777, root);
+	static int mkdir(const char *path, mode_t mode) {
 
-    second = create_directory("second");
-    root->n_links++; /* a new subfolder */
-    add_child_node(root, second);
+		int nameLen = 0;
+		for (int i = strlen(path) - 1; i >= 0; i--) {
+			if (path[i] == '/') break;
+			nameLen++;
+		}
 
-    first_inner = create_directory("first_inner");
-    first->n_links++; /* a new subfolder */
-    add_child_node(first, first_inner);
-    file = create_file("sample.txt");
-    file->n_links++; /* a new hard link */
-    add_child_node(root, file);
+		node_t *dir;
 
-    file->info.file.data_ptr = "Hello";
-    file->info.file.size = strlen(file->info.file.data_ptr);
+		if (nameLen != strlen(path) - 1) {
+			char* pathToDir = (char*)malloc(strlen(path) - nameLen - 1);
+			strncpy(pathToDir, path, strlen(path) - nameLen - 1);
+			pathToDir[strlen(path) - nameLen - 1] = '\0';
+			dir = findNode(pathToDir, root);
+		}
+		else {
+			dir = root;
+		}
 
-    fs_node_t* script = create_file("script.sh");
-    script->n_links++;
-    add_child_node(root, script);
-    script->info.file.data_ptr = "#!/bin/bash echo 123";
-    script->info.file.size = strlen(script->info.file.data_ptr);
-}
+		if (dir == NULL) return -1;
+
+		char *corrected_path = (char *)malloc(nameLen);
+		strncpy(corrected_path, path + strlen(path) - nameLen, nameLen);
+		corrected_path[nameLen] = '\0';
+		create_directory_and_append_to_parent(corrected_path, mode, dir);
+		return 0;
+	}
 
 
+	static void init() {
+		node_t *first, *second;
+		node_t *file;
+		node_t *first_inner;
 
-static int mkDir(const char *path, mode_t mode) {
-    printf("mk1dirdsaads:%s\n", path);
+		first = create_directory_and_append_to_parent("first", 0777, root);
 
-    int nameLen = 0;
-    for (int i = strlen(path) - 1; i >= 0; i--) {
-        if (path[i] == '/') break;
-        nameLen++;
-    }
+		second = create_directory("second");
+		root->links_count++; /* a new subfolder */
+		add_child_node(root, second);
 
-    printf("namelen %d \n", nameLen);
+		first_inner = create_directory("first_inner");
+		first->links_count++; /* a new subfolder */
+		add_child_node(first, first_inner);
+		file = create_file("sample.txt");
+		file->links_count++; /* a new hard link */
+		add_child_node(root, file);
 
-    fs_node_t *dir;
+		file->content.file.data = "Hello";
+		file->content.file.size = strlen(file->content.file.data);
 
-    if (nameLen != strlen(path) - 1) {
-        char* pathToDir = (char*) malloc(strlen(path) - nameLen - 1);
-        strncpy(pathToDir, path, strlen(path) - nameLen-1);
-        pathToDir[strlen(path) - nameLen - 1] = '\0';
-        printf("path to dir: %s\n", pathToDir);
-        dir = find_node(pathToDir, root);
-    } else {
-        dir = root;
-    }
+		node_t* script = create_file("script.sh");
+		script->links_count++;
+		add_child_node(root, script);
+		script->content.file.data = "#!/bin/bash echo 123";
+		script->content.file.size = strlen(script->content.file.data);
+	}
 
-    if (dir == NULL) return -1;
-
-    char *corrected_path = (char *) malloc(nameLen);
-    strncpy(corrected_path, path + strlen(path) - nameLen,  nameLen);
-    corrected_path[nameLen] = '\0';
-    printf("new dir name: %s\n", corrected_path);
-    create_dir(corrected_path, mode, dir);
-    return 0;
-}
-
-int main(int argc, char *argv[]) {
-    root = create_directory("");
-    make_sample_hierarchy();
-
-    return fuse_main(argc, argv, &operations, NULL);
-}
+	int main(int argc, char *argv[]) {
+		root = create_directory("");
+		init();
+		return fuse_main(argc, argv, &operations, NULL);
+	}
